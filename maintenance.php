d834e8ff33b91ff967337bb5b445b0c6e0ae865f
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-15 11:55:13 -0500

    Refactoring

---
+++ maintenance.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Modules manager admin actions
+ *
+ * @package    HNG2
+ * @subpackage modules_manager
+ * @author     Alejandro Caballero - lava.caballero@gmail.com
+ */
+
+use hng2_base\module;
+use hng2_cache\disk_cache;
+
+include "../config.php";
+include "../includes/bootstrap.inc";
+
+if( ! $account->_is_admin ) throw_fake_404();
+
+
+if( empty($_REQUEST["do_module_name"]) ) die($current_module->language->task_messages->module_not_provided);
+
+if( ! in_array($_REQUEST["install_action"], array("install", "uninstall", "enable", "disable")) )
+    die($current_module->language->task_messages->invalid_action);
+
+$do_module_name        = trim(stripslashes($_REQUEST["do_module_name"]));
+$module_install_action = trim($_REQUEST["install_action"]);
+
+if( ! is_file(ABSPATH . "/$do_module_name/module_info.xml"))
+    die($current_module->language->task_messages->module_not_found);
+
+$errors       = array();
+$messages     = array();
+$update_cache = false;
+
+include __DIR__ . "/actions/$module_install_action.inc";
+
+if( $update_cache )
+{
+    $modules_cache = new disk_cache("{$config->datafiles_location}/cache/modules.dat");
+    $module = new module(ABSPATH . "/{$do_module_name}/module_info.xml");
+    $modules_cache->set($do_module_name, $module->serialize());
+}
+
+if( count($messages) > 0 && count($errors) == 0 )
+{
+    send_notification($account->id_account, "success", implode("\n", $messages));
+    
+    die("OK");
+}
+
+if( count($messages) > 0 && count($errors) > 0 )
+{
+    $output = array();
+    $output[] = $current_module->language->messages->issues_detected . "\n";
+    $output[] = $current_module->language->messages->successfull_actions;
+    $output   = array_merge($output, $messages);
+    $output[] = "\n" . $current_module->language->messages->failed_actions;
+    $output   = array_merge($output, $errors);
+    $output[] = "\n" . $current_module->language->messages->please_review;
+    
+    die( implode("\n", $output) );
+}
+
+die( implode("\n", $errors) );
--- media/functions.js
+++ media/functions.js
@@ -1,20 +1,30 @@
 
 function do_module(action, module_name)
 {
-    location.href = $_PHP_SELF + '?install_action=' + action
-        + '&do_module_name=' + module_name
-        + '&tab=' + active_tab
-        + '&wasuuup=' + parseInt(Math.random() * 1000000000000000);
-}
-
-function reload_self()
-{
-    location.href = $_PHP_SELF + '?tab=' + active_tab + '&wasuuup=' + (Math.random() * 1000000000000000);
+    var url = 'maintenance.php?install_action=' + action
+            + '&do_module_name=' + module_name
+            + '&wasuuup=' + parseInt(Math.random() * 1000000000000000);
+    
+    stop_notifications_getter();
+    $.blockUI(blockUI_default_params);
+    $.get(url, function(response)
+    {
+        if( response != 'OK' )
+        {
+            $.unblockUI();
+            alert( response );
+            start_notifications_getter();
+            
+            return;
+        }
+        
+        reload_self();
+    });
 }
 
 function purge_modules_cache()
 {
-    var url = $_FULL_ROOT_PATH + '/modules_manager/purge_cache.php?wasuuup=' + parseInt(Math.random() * 1000000000000000);
+    var url = 'purge_cache.php?wasuuup=' + parseInt(Math.random() * 1000000000000000);
     $.blockUI(blockUI_default_params);
     $.get(url, function(response)
     {
@@ -32,7 +42,7 @@ function purge_modules_cache()
 
 function change_caching_status(new_status)
 {
-    var url = $_FULL_ROOT_PATH + '/modules_manager/change_caching_status.php';
+    var url = 'change_caching_status.php';
     var params = {
         'new_status': new_status,
         'wasuuup':    parseInt(Math.random() * 1000000000000000)
@@ -52,10 +62,7 @@ function change_caching_status(new_status)
     });
 }
 
-$(document).ready(function()
+function reload_self()
 {
-    $('#tabs').tabs({
-        active:   active_tab,
-        activate: function(event, ui) { active_tab = ui.newTab.index(); console.log(active_tab); }
-    });
-});
+    location.href = $_PHP_SELF + '?wasuuup=' + parseInt(Math.random() * 1000000000000000);
+}
