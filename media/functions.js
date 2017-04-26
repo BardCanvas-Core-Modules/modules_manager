24b87704d50b4b9cd0f401dc03d374797b2b842a
Alejandro Caballero <lava.caballero@gmail.com>
2017-04-26 17:13:44 -0500

    Added local search, tabs and group filters

--- media/functions.js
+++ media/functions.js
@@ -1,4 +1,55 @@
 
+function filter_modules()
+{
+    var group = get_current_group_name();
+    if( group != 'all_modules' ) toggle_modules('all_modules', false);
+    
+    var needle = $('#modules_filter').find('input[name="filter"]').val().trim().toLowerCase();
+    $('#modules_listing').find('.module').each(function()
+    {
+        var $this    = $(this);
+        var haystack = $(this).text().toLowerCase();
+        
+        if( haystack.indexOf(needle) >= 0 ) $this.show();
+        else                                $this.hide();
+    });
+}
+
+function clear_modules_filter()
+{
+    $('#modules_filter').find('input[name="filter"]').val('');
+    filter_modules();
+}
+
+function toggle_modules(key, clear_filter_before)
+{
+    if( typeof clear_filter_before == 'undefined' ) clear_filter_before = true;
+    
+    if( clear_filter_before ) $('#modules_filter').find('input[name="filter"]').val('');
+    
+    var $tabs = $('#flag_tabs');
+    $tabs.find('.tab').toggleClass('state_active', false);
+    $tabs.find('.tab[data-for="' + key + '"]').toggleClass('state_active', true);
+    $('#group_filter').find('option[value="' + key + '"]').prop('selected', true);
+    
+    $('#modules_listing').find('.module').each(function()
+    {
+        var $this = $(this);
+        if( $this.hasClass(key) ) $this.show();
+        else                      $this.hide();
+    });
+}
+
+function get_current_group_name()
+{
+    var group  = $('#group_filter').find('option:selected').val();
+    
+    if( group == 'all_modules' )
+        group = $('#flag_tabs').find('.tab.state_active').attr('data-for');
+    
+    return group;
+}
+
 function do_module(action, module_name)
 {
     var url = 'maintenance.php?install_action=' + action
@@ -64,5 +115,20 @@ function change_caching_status(new_status)
 
 function reload_self()
 {
-    location.href = $_PHP_SELF + '?wasuuup=' + wasuuup();
+    var group  = get_current_group_name();
+    var filter = $('#modules_filter').find('input[name="filter"]').val();
+    
+    location.href = $_PHP_SELF
+                  + '?filter=' + encodeURI(filter)
+                  + '&group='  + encodeURI(group)
+                  + '&wasuuup=' + wasuuup();
 }
+
+$(document).ready(function()
+{
+    var group = get_current_group_name();
+    if( group != 'all_modules' ) toggle_modules(group);
+    
+    if( $('#modules_filter').find('input[name="filter"]').val() != '' )
+        filter_modules();
+});
